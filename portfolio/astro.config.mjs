3ef37d73ad1cc0d6d0d9705cba9830de0d32a5df
Devendra Pratap Singh <dps.manit@gmail.com>
2026-06-09 11:12:51 +0530

    refactor: remove deprecated wrangler configuration files

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://dps.codes',
+  site: 'https://www.dps.codes',
   integrations: [sitemap()],
 });
