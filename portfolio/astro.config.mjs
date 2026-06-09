74fa550de55cfc23530d002ac5f595087a38c3b8
Devendra Pratap Singh <dps.manit@gmail.com>
2026-06-09 11:07:44 +0530

    feat: add session configuration to Astro setup

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://dps.codes',
   integrations: [sitemap()],
-  adapter: cloudflare(),
+  session: {
+    driver: null
+  },
+  adapter: cloudflare()
 });
